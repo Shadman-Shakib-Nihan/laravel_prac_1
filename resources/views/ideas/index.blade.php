af3d408ea1cf90ebb5a393a8dfcdaa5ef5c4609f
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-06-10 01:09:36 +0600

    Modify

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -1,21 +1,14 @@
 <x-layout>
 
-  <form method="POST" action="/ideas">
-    @csrf
-    <div class="col-span-full">
-          <label for="description" class="block text-sm/6 font-medium text-white">Ideas</label>
-          <div class="mt-2">
-            <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-gray-800 px-3 py-1.5 text-base text-gray-300 outline-1 -outline-offset-1 outline-gray-500 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
-          </div>
-          <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about your ideas.</p>
-        </div>
-   <div class="mt-6 flex items-center  gap-x-6">
-        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-  </div>
 
-</form>
 
 @if($ideas->count() > 0)
+  <div class="mt-6" text-white>
+    <h1 class="text-2xl font-bold text-white" >
+        Welcome to your Ideas
+    </h1>
+    <a href="/ideas/create" class="text-sm bg-green-600 hover:bg-green-500 px-2 py-1 rounded text-white mt-6">Create your  ideas</a> 
+  </div>
   <div class="mt-6" text-white>
     <h1 class="text-2xl font-bold text-white">
         Your Ideas
@@ -36,5 +29,12 @@
        @endforeach
     </div>
   </div> 
+@else
+  <div class="mt-6" text-white>
+    <h1 class="text-2xl font-bold text-white" >
+        No Ideas Yet
+    </h1>
+    <a href="/ideas/create" class="text-sm bg-green-600 hover:bg-green-500 px-2 py-1 rounded text-white">Create your first idea</a> 
+  </div>
 @endif
-</x-layout>
+</x-layout> 
