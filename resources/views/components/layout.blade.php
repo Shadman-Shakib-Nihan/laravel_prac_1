95cb2c434fd14bde266996e7dfcb0389d7553064
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-06-10 12:47:05 +0600

    Fixed UI

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,19 +3,22 @@
 ])
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="dark">
 <head>
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
  
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
  
 
 </head>
-<body class="bg-gray-700 p-6 max-w-xl mx-auto">   
+<body class="">
+    <x-nav />
     
-    <main>
+    <main class="max-w-3xl mx-auto p-6">
         {{ $slot }}
     </main>
 </body>
--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -13,20 +13,40 @@
     <h1 class="text-2xl font-bold text-white">
         Your Ideas
     </h1>
-    <div class="mt-4 space-y-3">
+
+    <div class="mt-4 space-y-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+  
        @foreach ($ideas as $idea)
-        <div class="flex items-center justify-between bg-gray-800 p-3 rounded-md">
-          <span class="text-sm text-gray-300">{{$idea->description}}</span>
-          <div class="flex gap-2">
-            <a href="/ideas/{{ $idea->id }}/edit" class="text-xs bg-blue-600 hover:bg-blue-500 px-2 py-1 rounded text-white">Edit</a>
-            <form method="POST" action="/ideas/{{ $idea->id }}" style="display:inline;">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="text-xs bg-red-600 hover:bg-red-500 px-2 py-1 rounded text-white">Delete</button>
-            </form>
-          </div>
+
+<div class="card bg-neutral text-neutral-content shadow-md h-64">
+    <div class="card-body flex flex-col h-full">
+        
+        <div class="overflow-y-auto flex-1">
+            <p class="text-sm break-words">
+                {{ $idea->description }}
+            </p>
         </div>
+
+        <div class="card-actions justify-end mt-2">
+            <a href="/ideas/{{ $idea->id }}/edit" class="btn btn-primary btn-sm">
+                Edit
+            </a>
+
+            <form method="POST" action="/ideas/{{ $idea->id }}">
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-error btn-sm">
+                    Delete
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+ 
        @endforeach
+       
     </div>
   </div> 
 @else
