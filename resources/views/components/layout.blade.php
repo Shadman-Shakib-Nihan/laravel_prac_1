91719808891595578eff9182624fb33d99f77a4c
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-06-14 16:06:39 +0600

    local css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,19 +5,17 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="dark">
 <head>
-    <meta charset="UTF-8"> 
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
- 
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
- 
+
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
 </head>
-<body class=""> 
+<body class="text-primary">
     <x-nav />
-    
+
     <main class="max-w-3xl mx-auto p-6">
         {{ $slot }}
     </main>
